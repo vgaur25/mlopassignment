1dd4a06482d3ba70fb65d7822a488d18c22b9ce1
vgaur25 <vgaur25@gmail.com>
2025-01-27 18:15:24 +0530

    Fixed

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,16 +1,14 @@
-
-
-from  src.math_operation import add, sub
+from src.math_operation import add, sub
 
 
 def test_add():
-    assert add(2, 5)==7
-    assert add(-1, 1)==0
-    assert add(6, 1)==7
+    assert add(2, 5) == 7
+    assert add(-1, 1) == 0
+    assert add(6, 1) == 7
 
 
 def test_sub():
-    assert sub(5, 3)==2
-    assert sub(4, 3)==1
-    assert sub(3, 3)==0
-    assert sub(2, 3)==-1
+    assert sub(5, 3) == 2
+    assert sub(4, 3) == 1
+    assert sub(3, 3) == 0
+    assert sub(2, 3) == -1
